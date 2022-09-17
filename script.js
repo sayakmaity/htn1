9b307d80e7ef64c096b56792ea324369a6d4b83f
Sayak Maity <smaity@college.harvard.edu>
2022-09-17 18:40:27 -0400

    cock

--- script.js
+++ script.js
@@ -1,3 +1,46 @@
+const { Client } = require('pg')
+
+const client = new Client(process.env.DATABASE_URL)
+
+client.connect()
+
+
+
+
+// javascript login system
+function login() {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    if (username == "admin" && password == "admin") {
+        alert ("Login successfully");
+        window.location = "success.html"; // Redirecting to other page.
+        return false;
+    }
+    else {
+        alert("Wrong username or password");
+    }
+}
+
+// take name as javascript input and add it to cockroachdb
+function addName() {
+    var name = document.getElementById("name").value;
+    var query = "INSERT INTO names (name) VALUES ('" + name + "')";
+    client.query(query, (err, res) => {
+        if (err) {
+            console.log(err.stack)
+        } else {
+            console.log(res.rows[0])
+        }
+    })
+}
+
+
+
+
+
+
+
+
 var username = document.getElementById("username");
 var pass = document.getElementById("pass");
 pass.onfocus = function () {
